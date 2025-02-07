1ab6fda67b06ac50b27237e683ddee40ccd2660d
James Graham <james@hoppipolla.co.uk>
2025-02-07 11:26:50 +0000

    Add CI tests

---
+++ ci/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ex
+
+REL_DIR_NAME=$(dirname "$0")
+SCRIPT_DIR=$(cd "$REL_DIR_NAME" && pwd -P)
+cd "$SCRIPT_DIR"/..
+
+cargo test
+
+cd python
+uv run mypy python/wpt_interop/
+uv run ruff check
+uv run ruff format --check
